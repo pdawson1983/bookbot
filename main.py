e873274063776fd4a9a5220131bf6f62b04fa289
Paul Dawson <pdawson1983@gmail.com>
2025-02-09 18:39:43 -0500

    correct display to only show header and not whole book

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def main(book):
-    book = get_book_string("books/frankenstein.txt")
-    display_header(book)
+def main(book_location="books/frankenstein.txt"):
+    book = get_book_string(book_location)
+    display_header(book_location)
     print(f"{count_words(book)} words in the book.")
-    print(count_char(book))
+    display_char(count_char_dict(book))
 
 
 def display_header(book_path):
@@ -19,7 +19,7 @@ def get_book_string(book_location):
 def count_words(book_string):
     return len(book_string.split())
 
-def count_char(book_string):
+def count_char_dict(book_string):
     character_dict = {}
     for character in book_string.lower():
         if character in character_dict:
@@ -27,5 +27,9 @@ def count_char(book_string):
         else:
             character_dict[character] = 1
     return character_dict
+
+def display_char(char_dict):
+    for character in char_dict:
+        print(f"The'{character}' character was found {char_dict[character]} times")
 
 main()
